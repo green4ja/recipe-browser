c7d2579b3b1296aa13a6333a7cb2e283373a36de
green4ja <green4ja@mail.uc.edu>
2025-08-11 17:20:53 -0400

    updated all_possible_ingredients.json to all lowercase characters

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ Used for writing all possible ingredients to the all_possible_ingredients.json
 
 r = requests.get('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
 r_json = r.json()
-li = [item.get('strIngredient') for item in r_json['meals']]
+li = [item.get('strIngredient').lower() for item in r_json['meals']]
 
 try:
     with open(file='data/all_possible_ingredients.json', mode='w', encoding='utf-8') as file:
