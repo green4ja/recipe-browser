a633cee282ea0afef80b7b776169d3a21f778fde
green4ja <green4ja@mail.uc.edu>
2025-08-13 13:00:31 -0400

    test logic for adding new recipe in test.py, new data storage file complete_recipes.json

--- GUI.py
+++ GUI.py
@@ -16,10 +16,36 @@ from PyQt5.QtCore import Qt  # Qt used for alignments
 from FileManager import FileManager
 
 
+class ScrollableIngredientChecklist(QListWidget):
+    def __init__(self):
+        super().__init__()  # Call parent's __init__ (QListWidget)
+        self.FileManager = FileManager()
+        self.ingredient_list_widget = QListWidget(self)
+        self.actual_ingredient_list = self.FileManager.return_all_possible_ingredients()
+        for ingredient in self.actual_ingredient_list:
+            item = QListWidgetItem(
+                ingredient
+            )  # creates new list widget item with text set to current ingredient name
+            item.setFlags(
+                item.flags() | Qt.ItemIsUserCheckable
+            )  # adds a checkbox to each ingredient
+            item.setCheckState(
+                Qt.Checked
+            )  # sets initial state of checkbox to "unchecked"
+            self.ingredient_list_widget.addItem(
+                item
+            )  # adds the configured widget item to ingredient_list_widget
+
+
+class ScrollableRecipeList(QListWidget):
+    def __init__(self):
+        pass
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.FileManager = FileManager()
+        super().__init__()  # Call parent's __init__ (QMainWindow)
+        self.ScrollableIngredientChecklist = ScrollableIngredientChecklist()
         self.setWindowTitle("recipe-browser")  # set the window title
         self.setGeometry(
             480, 270, 960, 540
@@ -32,27 +58,14 @@ class MainWindow(QMainWindow):
     def initUI(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-
-        # Create Ingredient List Widget
-        ingredient_list_widget = QListWidget(self)
-        actual_ingredient_list = self.FileManager.return_all_possible_ingredients()
-        for ingredient in actual_ingredient_list:
-            item = QListWidgetItem(
-                ingredient
-            )  # creates new list widget item with text set to current ingredient name
-            item.setFlags(
-                item.flags() | Qt.ItemIsUserCheckable
-            )  # adds a checkbox to each ingredient
-            item.setCheckState(
-                Qt.Unchecked
-            )  # sets initial state of checkbox to "unchecked"
-            ingredient_list_widget.addItem(
-                item
-            )  # adds the configured widget item to ingredient_list_widget
+        scrollable_ingredient_checklist = (
+            self.ScrollableIngredientChecklist.ingredient_list_widget
+        )
+        scrollable_ingredient_checklist.setFixedSize(290, 480)  # width, height
 
         # Grid Layout
         grid = QGridLayout()
-        grid.addWidget(ingredient_list_widget, 0, 0)  # widget, row, col
+        grid.addWidget(scrollable_ingredient_checklist, 0, 0)  # widget, row, col
 
         central_widget.setLayout(grid)
 
--- test.py
+++ test.py
@@ -1,16 +1,67 @@
 import requests
 import json
+from API import API
 
 """
-Used for writing all possible ingredients to the all_possible_ingredients.json
+Used for testing functionality.
 """
 
-r = requests.get('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
-r_json = r.json()
-li = [item.get('strIngredient').lower() for item in r_json['meals']]
+a = API()
+meal = a.get_random_recipe()['meals'][0]
+# r = requests.get('https://www.themealdb.com/api/json/v1/1/random.php')
+# r_json = r.json()
+# meal = r_json['meals'][0]
+# ingredient_name_list = [
+#     value.lower()
+#     for key, value in meal.items()
+#     if key.startswith('strIngredient') and value and value.strip()
+# ]
 
+# ingredient_measure_list = [
+#     value.lower()
+#     for key, value in meal.items()
+#     if key.startswith('strMeasure') and value and value.strip()
+# ]
+
+recipe_data = {
+    "name": meal['strMeal'],
+    "instructions": meal['strInstructions'],
+    "youtubeVideoURL": meal['strYoutube'],
+}
+
+ingredients = []
+for i in range(1, 21):
+    ingredient = meal.get(f"strIngredient{i}")
+    measure = meal.get(f"strMeasure{i}")
+    if ingredient and ingredient.strip():
+        ingredients.append({
+            "name": ingredient.lower(),
+            "measure": measure.strip() if measure else ""
+        })
+        print(f"i = {i}")
+    else:
+        break # exit loop on first empty ingredient
+
+recipe_data["ingredients"] = ingredients
+
+print(meal)
+print(recipe_data)
+# print(ingredient_name_list)
+# print(ingredient_measure_list)
+
+# Load recipes
 try:
-    with open(file='data/all_possible_ingredients.json', mode='w', encoding='utf-8') as file:
-        json.dump(li, file, indent=4)
+    with open(file="data/complete_recipes.json", mode="r", encoding="utf-8") as file1:
+        recipes = json.load(file1)
+except Exception as e:
+    recipes = []
+    print(e)
+
+recipes.append(recipe_data)
+
+# Add new recipe
+try:
+    with open(file="data/complete_recipes.json", mode="w", encoding="utf-8") as file2:
+        json.dump(recipes, file2, indent=4)
 except Exception as e:
     print(e)
